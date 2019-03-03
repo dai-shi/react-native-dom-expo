7e449171cd9b026b21fb274b4bd3319ef74a876e
daishi <daishi@axlight.com>
2019-03-03 23:05:05 +0900

    read manifest from app.json

--- src/ExponentConstants.dom.js
+++ src/ExponentConstants.dom.js
@@ -1,11 +1,14 @@
 import ExponentConstants from '../../expo-constants/build/ExponentConstants.web';
 
+import appJson from '../../../app.json';
+
 export default {
   ...ExponentConstants,
   platform: {
     dom: ExponentConstants.platform.web,
   },
   manifest: {
+    ...appJson.expo,
     ...(__DEV__ && {
       developer: {},
       bundleUrl: 'http://localhost:8081/dom/',
